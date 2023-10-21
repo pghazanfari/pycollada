af5ac9baffb8efb176ccd4471912e6c9342dfacd
Parham Ghazanfari <pg53@vt.edu>
2023-10-21 16:37:02 -0400

    Added support for parsing more animation nodes

--- collada/animation.py
+++ collada/animation.py
@@ -16,13 +16,101 @@ from collada import source
 from collada.common import DaeObject
 from collada.common import DaeError
 
+class INTERPOLATION:
+    """An enum of the interpolation methods supported by COLLADA"""
+    LINEAR = 'LINEAR'
+    BEZIER = 'BEZIER'
+    CARDINAL = 'CARDINAL'
+    HERMITE = 'HERMITE'
+    BSPLINE = 'BSPLINE'
+    STEP = 'STEP'
+
+class SEMANTIC:
+    """An enum of the semantic methods supported by COLLADA"""
+    INPUT = 'INPUT'
+    OUTPUT = 'OUTPUT'
+    INTERPOLATION = 'INTERPOLATION'
+
+class Input(DaeObject):
+    """Class for holding animation sampler input coming from <input> tags."""
+    def __init__(self, semantic, source):
+        self.semantic = semantic
+        self.source = source
+
+    @staticmethod
+    def load(collada, localscope, node):
+        semantic = node.get('semantic')
+        source = node.get('source')
+        if not semantic or not source:
+            raise DaeError("Input node of animation sampler missing semantic or source")
+        if source[1:] not in localscope:
+            raise DaeError("Input of animation sampler refering to source '%s' not found" % source)
+        print("input.source=", localscope[source[1:]])
+        return Input(semantic, localscope[source[1:]])
+
+class Sampler(DaeObject):
+    """Class for holding animation sampling coming from <sampler> tags."""
+    
+    def __init__(self, id, inputs):
+        self.id = id
+        self.inputs = inputs
+
+    @property
+    def input(self):
+        return next(x for x in self.inputs if x.semantic == SEMANTIC.INPUT)
+
+    @property
+    def output(self):
+        return next(x for x in self.inputs if x.semantic == SEMANTIC.OUTPUT)
+
+    @property
+    def interpolation(self):
+        return next(x for x in self.inputs if x.semantic == SEMANTIC.INTERPOLATION)
+
+    @staticmethod
+    def load( collada, localscope, node ):
+        id = node.get('id') or ''
+        
+        inputs = []
+        for input_node in node.findall(collada.tag('input')):
+            inputs.append(Input.load(collada, localscope, input_node))
+        
+        return Sampler(id, inputs)
+    
+    def __str__(self): return '<Sampler id=%s>' % (self.id,)
+    def __repr__(self): return str(self)
+
+class Channel(DaeObject):
+    """Class for holding animation channel coming from <channel> tags."""
+    
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+    
+    @staticmethod
+    def load( collada, localscope, node ):
+        source = node.get('source')
+        target = node.get('target')
+        
+        if not source or not target:
+            raise DaeError("Animation channel node missing source or target")
+        if source[1:] not in localscope:
+            raise DaeError("Input of animation channel '%s' not found" % (source))
+        
+        source = localscope[source[1:]]
+        return Channel(source, target)
+    
+    def __str__(self): return '<Channel source=%s target=%s>' % (self.source, self.target)
+    def __repr__(self): return str(self)
 
 class Animation(DaeObject):
     """Class for holding animation data coming from <animation> tags."""
 
-    def __init__(self, id, name, sourceById, children, xmlnode=None):
+    def __init__(self, id, name, sourceById, samplers, channels, children, xmlnode=None):
         self.id = id
         self.name = name
+        self.samplers = samplers
+        self.channels = channels
         self.children = children
         self.sourceById = sourceById
         self.xmlnode = xmlnode
@@ -42,6 +130,21 @@ class Animation(DaeObject):
             sources.append(ch)
             sourcebyid[ch.id] = ch
 
+        samplers = {}
+        for sampler_node in node.findall(collada.tag('sampler')):
+            try:
+                sampler = Sampler.load(collada, sourcebyid, sampler_node)
+                samplers[sampler.id] = sampler
+            except DaeError as ex:
+                collada.handleError(ex)
+
+        channels = []
+        for channel_node in node.findall(collada.tag('channel')):
+            try:
+                channels.append(Channel.load(collada, samplers, channel_node))
+            except DaeError as ex:
+                collada.handleError(ex)
+
         child_nodes = node.findall(collada.tag('animation'))
         children = []
         for child in child_nodes:
@@ -51,7 +154,7 @@ class Animation(DaeObject):
             except DaeError as ex:
                 collada.handleError(ex)
 
-        anim = Animation(id, name, sourcebyid, children, node)
+        anim = Animation(id, name, sourcebyid, list(samplers.values()), channels, children, node)
         return anim
 
     def __str__(self):
